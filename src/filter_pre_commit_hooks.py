c9d383206ad2d84d64e0c99b87af56accaaa19b8
Tim Schwenke <tim@trallnag.com>
2025-01-04 14:14:17 +0100

    Adjust license header

--- src/filter_pre_commit_hooks.py
+++ src/filter_pre_commit_hooks.py
@@ -1,5 +1,9 @@
 #
-# ISC
+# /// script
+# requires-python = ">=3.10"
+# ///
+#
+# This work is available under the ISC license.
 #
 # Copyright Tim Schwenke <tim@trallnag.com>
 #
@@ -14,10 +18,6 @@
 # LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
 # OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
-#
-# /// script
-# requires-python = ">=3.10"
-# ///
 #
 
 import re
