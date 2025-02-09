2a01fe1d207873acaf9136b2f11ddef6a8a0abca
Tim Schwenke <tim@trallnag.com>
2025-02-09 18:10:06 +0100

    Switch from uv to shebang execution

--- scripts/run-pre-commit-checks.bash
+++ scripts/run-pre-commit-checks.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 
 declare -x SKIP
 
-SKIP=$(uv run --script scripts/filter_pre_commit_hooks.py check task)
+SKIP=$(./scripts/filter_pre_commit_hooks.py check task)
 
 pre-commit run --all-files \
   | grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
--- scripts/run-pre-commit-fixes.bash
+++ scripts/run-pre-commit-fixes.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 
 declare -x SKIP
 
-SKIP=$(uv run --script scripts/filter_pre_commit_hooks.py fix task)
+SKIP=$(./scripts/filter_pre_commit_hooks.py fix task)
 
 (pre-commit run --all-files || pre-commit run --all-files) \
   | grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
