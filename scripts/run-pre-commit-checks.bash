a77ff9f74704c4952325557543176735bacd603f
Tim Schwenke <tim@trallnag.com>
2025-02-11 15:52:39 +0000

    Simplify grep regexp
    No need to check for espace. Just never show if skipped.

--- scripts/run-pre-commit-checks.bash
+++ scripts/run-pre-commit-checks.bash
@@ -11,4 +11,4 @@ declare -x SKIP
 SKIP=$(./scripts/filter_pre_commit_hooks.py check task)
 
 pre-commit run --all-files \
-  | grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
+  | grep --perl-regexp --invert-match --regexp='Skipped'
--- scripts/run-pre-commit-fixes.bash
+++ scripts/run-pre-commit-fixes.bash
@@ -14,4 +14,4 @@ declare -x SKIP
 SKIP=$(./scripts/filter_pre_commit_hooks.py fix task)
 
 (pre-commit run --all-files || pre-commit run --all-files) \
-  | grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
+  | grep --perl-regexp --invert-match --regexp='Skipped'
