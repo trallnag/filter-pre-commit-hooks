26393e2ac5123ae8d9f4c67618ff4f1bdecd49b3
Tim Schwenke <tim@trallnag.com>
2025-02-12 16:15:18 +0100

    Handle grep finding nothing

--- scripts/run-pre-commit-checks.bash
+++ scripts/run-pre-commit-checks.bash
@@ -11,4 +11,4 @@ declare -x SKIP
 SKIP=$(./scripts/filter_pre_commit_hooks.py check task)
 
 pre-commit run --all-files \
-  | grep --perl-regexp --invert-match --regexp='Skipped'
+  | (grep --invert-match --regexp='Skipped' || true)
--- scripts/run-pre-commit-fixes.bash
+++ scripts/run-pre-commit-fixes.bash
@@ -2,16 +2,16 @@
 
 #
 # This script runs pre-commit hooks that are tagged with "fix" and "task".
-#i
+#
 # Pre-commit is executed twice so that the script only fails if there is
 # something really wrong and not just a successful fix.
 #
 
-set -euo pipefail
+set -xeuo pipefail
 
 declare -x SKIP
 
 SKIP=$(./scripts/filter_pre_commit_hooks.py fix task)
 
 (pre-commit run --all-files || pre-commit run --all-files) \
-  | grep --perl-regexp --invert-match --regexp='Skipped'
+  | (grep --invert-match --regexp='Skipped' || true)
