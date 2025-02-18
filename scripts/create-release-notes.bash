9d5ed0e884a40e731c54f100d31cd625efc05462
Tim Schwenke <tim@trallnag.com>
2025-02-18 23:58:05 +0100

    Move from tmp to .tmp

--- scripts/create-release-notes.bash
+++ scripts/create-release-notes.bash
@@ -2,20 +2,20 @@
 
 #
 # This script generates the release notes for the latest release by extracting
-# them from the changelog, formatting them, and writing them to a file in tmp.
+# them from the changelog, formatting them, and writing them to a file.
 #
 
 set -euo pipefail
 
-mkdir -p tmp
+mkdir -p .tmp
 
-release_notes=tmp/release-notes.md
+release_notes=.tmp/release-notes.md
 
 awk '/^## /{count++} count==2{print} count==3{exit}' CHANGELOG.md \
   | tail +2 \
   | awk 'NF {p=1} p' | tac \
   | awk 'NF {p=1} p' | tac \
   | sed 's/^### /## /' \
-    > $release_notes
+    > "$release_notes"
 
-mdformat --wrap=no $release_notes
+mdformat --wrap=no "$release_notes"
